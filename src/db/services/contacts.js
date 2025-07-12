c9cee66858e812a9c5065b78397eda675298ab58
IrynaYakymenko <irynayakymenko93@gmail.com>
2025-07-12 10:09:26 +0300

    addFindOne

--- src/db/services/contacts.js
+++ src/db/services/contacts.js
@@ -37,26 +37,28 @@ export const getContacts = async ({
   };
 };
 
-export const getContactById = (id) => Contact.findById(id);
+export const getContactById = (id, userId) =>
+  Contact.findOne({ _id: id, userId });
 
 export const createContact = async (payload) => {
   const contact = await Contact.create(payload);
   return contact;
 };
 
-export const deleteContact = async (id) => {
-  const contact = await Contact.findOneAndDelete({
-    _id: id,
-  });
-  return contact;
+export const deleteContact = async (id, userId) => {
+  return await Contact.findOneAndDelete({ _id: id, userId });
 };
 
-export const updateContact = async (id, payload, options = {}) => {
-  const rawResult = await Contact.findOneAndUpdate({ _id: id }, payload, {
-    new: true,
-    includeResultMetadata: true,
-    ...options,
-  });
+export const updateContact = async (id, userId, payload, options = {}) => {
+  const rawResult = await Contact.findOneAndUpdate(
+    { _id: id, userId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
 
   if (!rawResult || !rawResult.value) return null;
 
