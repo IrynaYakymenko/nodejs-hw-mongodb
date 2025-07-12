7cb9adbdf870403dd7382777695dbbc93b0cbc16
IrynaYakymenko <irynayakymenko93@gmail.com>
2025-07-12 21:46:58 +0300

    addlink

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,10 +12,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/isValidId.js';
 
-import {
-  createContactSchema,
-  updateContactSchema,
-} from '../validation/contacts.js';
+import { createContactSchema } from '../validation/contacts.js';
 
 import { authenticate } from '../middlewares/authenticate.js';
 import { upload } from '../middlewares/multer.js';
